a033ada408fa7f67ec0e2d322d02ac617b530fad
unknown <sumandrita.mouli@gmail.com>
2021-05-30 16:35:23 +0530

    3rd commit

--- src/concateStrings.java
+++ src/concateStrings.java
@@ -3,16 +3,16 @@ import java.util.*;
 public class concateStrings {
 	public static void main(String[] args) {
 		String sb = "";
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter number of string want to append : ");
-		int n = sc.nextInt();
-		for (int i = 0; i < n; i++) {
-			System.out.println("enter the string : ");
-			String str = sc.next();
-			sb += str;
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter number of string want to append : ");
+			int n = sc.nextInt();
+			for (int i = 0; i < n; i++) {
+				System.out.println("enter the string : ");
+				String str = sc.next();
+				sb += str;
 
+			}
 		}
-
 		System.out.println("The concatenated string is : "+sb);
 
 	}
